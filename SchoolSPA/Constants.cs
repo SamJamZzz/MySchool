170bca65e954995aa5363ecb54ff960f37069728
Sam Julian <samjulian6@gmail.com>
2024-03-09 23:41:06 -0800

    Add QuickGrid

--- SchoolSPA/Constants.cs
+++ SchoolSPA/Constants.cs
@@ -4,5 +4,5 @@ public class Constants
 {
     //public const string SchoolAPIBase = "http://localhost:5208/api/";
     //public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
-    public const string SchoolAPIBase = "/api/";
+    public const string SchoolAPIBase = "https://functions4870mze.azurewebsites.net/api/";
 }
